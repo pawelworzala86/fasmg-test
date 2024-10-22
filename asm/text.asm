353f139d931af40b86ddcdeef8f04c61e0cef06f
Paweł Worzała <pawelworzala86@gmail.com>
2024-10-22 16:58:38 +0200

    auto save

--- asm/text.asm
+++ asm/text.asm
@@ -8,38 +8,21 @@ entry start
 
 ;include 'include\\opengl.inc'
 
+
 compare_strings:
-    ;push rbp
-    ;mov rbp, rsp
-    ;sub rsp, 8*2
-
-    ;invoke printf, "%s %s", [rbp+24], [rbp+16]
-
-    ;mov rsi, [rbp+24]
-    ;mov rdi, [rbp+32]
-    cld               ; Clear direction flag
-
+    cld
 compare_loop:
-    lodsb             ; Load byte from str1 into AL
-    scasb             ; Compare byte in AL with byte at str2
-    jne strings_not_equal ; If not equal, jump to the label
-    test al, al       ; Check if end of string (null byte)
-    jnz compare_loop  ; If not end, continue loop
-
+    lodsb            
+    scasb             
+    jne strings_not_equal
+    test al, al
+    jnz compare_loop
 strings_equal:
-    mov rax, 1        ; Strings are equal
-
-    ;mov rsp, rbp
-    ;pop rbp
+    mov rax, 1
     ret
-
 strings_not_equal:
-    mov rax, 0        ; Strings are not equal
-
-    ;mov rsp, rbp
-    ;pop rbp
+    mov rax, 0
     ret
-
 macro CompareText strA, strB
     mov rsi, strA
     mov rdi, strB
