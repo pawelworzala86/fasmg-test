b0420b532d9c2c86132c3f57a481a028a49249a9
Paweł Worzała <pawelworzala86@gmail.com>
2024-10-22 17:04:52 +0200

    auto save

--- asm/text.asm
+++ asm/text.asm
@@ -30,6 +30,44 @@ macro CompareText strA, strB
 end macro
 
 
+copy_string:
+    ;mov rsi, src      ; adres źródłowego ciągu
+    ;mov rdi, dest     ; adres docelowego ciągu
+    cld               ; Clear direction flag
+copy_loop:
+    lodsb             ; Załaduj bajt z src do AL
+    stosb             ; Zapisz bajt z AL do dest
+    test al, al       ; Sprawdź, czy to koniec ciągu
+    jnz copy_loop     ; Jeśli nie, kontynuuj pętlę
+ret
+macro CopyText src, dest
+    mov rsi, src
+    mov rdi, dest
+    call copy_string
+end macro
+
+
+
+string_length:
+    ;mov rsi, str      ; adres ciągu
+    xor rcx, rcx      ; wyzeruj licznik
+
+length_loop:
+    lodsb             ; załaduj bajt z ciągu do AL
+    test al, al       ; sprawdź, czy to koniec ciągu
+    je done           ; jeśli tak, przejdź do końca
+    inc rcx           ; zwiększ licznik
+    jmp length_loop   ; kontynuuj pętlę
+
+done:
+    mov rax, rcx      ; wynik w RAX
+ret
+macro TextLength src
+    mov rsi, src
+    call string_length
+end macro
+
+
 
 
 
@@ -42,7 +80,13 @@ section '.text' code readable executable
     ;mov rdi, string2
     ;call compare_strings
     CompareText string1, string2
-    invoke printf, "%i OK", rax
+    invoke printf, "%i", rax
+
+    CopyText sourceString, destString
+    invoke printf, "%s", destString
+
+    TextLength string1
+    invoke printf, "%i", rax
 
     invoke	ExitProcess,0
 
@@ -51,6 +95,9 @@ section '.data' data readable writeable
 
     string1 db 'Hello', 0
     string2 db 'Hello', 0
+
+    sourceString db 'Hello, World!', 0
+    destString db 20 dup(0)
 
 
 section '.idata' import data readable writeable
